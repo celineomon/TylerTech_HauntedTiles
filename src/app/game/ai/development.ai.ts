7c7a94f87523690ab5adc0cb530bd4f5fa887248
Celine Nakamura <nakamura.celine@gmail.com>
2020-11-12 16:43:51 -1000

    Updated code

--- src/app/game/ai/development.ai.ts
+++ src/app/game/ai/development.ai.ts
@@ -8,49 +8,141 @@ side will tell you which team is yours
 main should return an array of MoveDirection's (also found in interfaces.ts) with size = # of monsters on one team to start
 
 Take a look at other example scripts to get some ideas on how to leverage this data
-
-export interface IGameState {
-  boardSize: [number, number];
-  tileStates: TileState[][];
-  teamStates: TeamStates;
-}
-
-export enum TileState {
-  Good = 3,
-  Warning = 2,
-  Danger = 1,
-  Broken = 0,
-}
-
-export enum Side {
-  Home = 'home',
-  Away = 'away',
-}
-
-export enum MoveDirection {
-  North = 'north',
-  South = 'south',
-  East = 'east',
-  West = 'west',
-  None = 'none',
-}
-
-export type TeamStates = Record<Side, ITeamMemberState[]>;
-
-export interface ITeamMemberState {
-  coord: Coord;
-  isDead: boolean;
-}
-
-export type Coord = [number, number];
-
-
 */
 
 // do not include "export const developmentScript = " with your submission
 export const developmentScript = `
+let board = [];
+let turn = -1;
+let prevOtherMoves = [];
+let otherMoves = [];
+let curMoves = [];
 function main(gameState, side) {
-    const myTeam = gameState.teamStates[side];
-    return ['none', 'none', 'none'];
-  }
+	turn++;
+	// console.log("Size", board.length)
+	const [rowSize, colSize] = gameState.boardSize;
+	const myTeam = gameState.teamStates[side];
+	const otherSide = side === 'home' ? 'away' : 'home';
+	const otherTeam = gameState.teamStates[otherSide];
+	const possibleMoves = [];
+
+	if (turn === 0) {
+		for(let i = 0; i < rowSize; i++) {
+			board.push([]);
+			for(let j = 0; j < colSize; j++) {
+				if(gameState.tileStates[i][j] == 0) {
+					board[i].push(0);
+				} else {
+					board[i].push(3);
+				}
+			}
+		}
+		for(let i = 0; i < myTeam.length; i++) {
+			curMoves.push(myTeam[i].coord);
+			// console.log(curMoves[i])
+		}
+		// console.log(board)
+	}
+
+	console.log('turn', turn);
+	// console.log(gameState.tileStates)
+	return new Promise((resolve, reject) => {
+		prevOtherMoves = otherMoves;
+		otherMoves = otherTeam.map(member => {
+			return member.coord;
+		});
+		
+		if (turn === 0) {
+			prevOtherMoves = otherMoves;
+		}
+		// console.log('prev', prevOtherMoves)
+		// console.log('other', otherMoves)
+
+		otherMoves.forEach((m, i) => {
+			const [curRow, curCol] = m;
+			const [prevRow, prevCol] =  prevOtherMoves[i];
+			// board[prevRow][prevCol]--;
+			if(curRow !== prevRow || curCol !== prevCol) {
+				// console.log('other moved', curRow, curCol);
+				board[curRow][curCol]--;
+			}
+		});
+
+		const callback = () => resolve(
+			myTeam.reduce((moveSet, member, i) => {
+				if (member.isDead) {
+					moveSet.push('none');
+				} else {
+					const [row, col] = curMoves[i];
+					console.log('premove', row, col);
+
+					const canNorth = row > 0;
+					const canSouth = row < rowSize - 1;
+					const canWest = col > 0;
+					const canEast = col < colSize - 1;
+					const northExist = board[row - 1 > 0 ? row - 1 : 0][col] > 1;
+					const southExist = board[row + 1 < 6 ? row + 1 : 6][col] > 1;
+					const westExist  = board[row][col - 1 > 0 ? col - 1 : 0] > 1;
+					const eastExist  = board[row][col + 1 < 6 ? col + 1 : 6] > 1;
+					const idleExist  = board[row][col] > 2;
+					console.log('N', canNorth, 'S', canSouth, 'W', canWest, 'E', canEast);
+					console.log('N', northExist, 'S', southExist, 'W', westExist, 'E', eastExist);
+					console.log(
+					  'N', board[row - 1 >= 0 ? row - 1 : 0][col],
+					  'S', board[row + 1 <= 6 ? row + 1 : 6][col],
+					  'W', board[row][col - 1 >= 0 ? col - 1 : 0],
+					  'E', board[row][col + 1 <= 6 ? col + 1 : 6],
+					);
+
+					if (canNorth && northExist) possibleMoves.push('north');
+					if (canSouth && southExist)  possibleMoves.push('south');
+					if (canWest && westExist) possibleMoves.push('west');
+					if (canEast && eastExist)  possibleMoves.push('east');
+					if (idleExist) possibleMoves.push('none');
+					
+					if(possibleMoves.length == 0) possibleMoves.push('none');
+					const move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+					console.log(move, possibleMoves)
+
+					let newRow = row, newCol = col;
+					switch(move) {
+						case 'north': {
+							newRow = row - 1 > 0 ? row - 1 : 0;
+							break;
+						}
+						case 'south': {
+							newRow = row + 1 < 6 ? row + 1 : 6;
+							break;
+						}
+						case 'west': {
+							newCol = col - 1 > 0 ? col - 1 : 0;
+							break;
+						}
+						case 'east': {
+							newCol = col + 1 < 6 ? col + 1 : 6;
+							break;
+						}
+						default: {
+							break;
+						}
+					}
+					// if (move !== 'none') {
+						console.log('moved', move, row + '->' + newRow, col + '->' + newCol, board[newRow][newCol]);
+						board[newRow][newCol]--;
+						curMoves[i] = [newRow, newCol];
+					// }
+					moveSet.push(move);
+					// console.log('row 0', board[0]);
+					// console.log('row 1', board[1]);
+					// console.log('row 2', board[2]);
+					// console.log('row 3', board[3]);
+					// console.log('row 4', board[4]);
+					possibleMoves.length = 0;
+				}
+				return moveSet;
+			}, [])
+		);
+		return callback();
+		})
+}
 `;
